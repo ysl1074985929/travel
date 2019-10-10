23e97460f212a767e6ee8dd21ecf404ee002b28d
yushilong <1074985929@qq.com>
2019-10-10 12:13:25 +0800

    轮播图

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <swiper-slide v-for="item of swiperList" :key='item.id'>
+                <img class="swiper-img" :src="item.imgUrl">
+            </swiper-slide>
+            
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeSwiper',
+    data: function(){
+        return{
+            swiperOption: {
+                pagination: '.swiper-pagination',
+                loop: true
+            },
+            swiperList: [{
+                id: '0001',
+                imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20198/06e65ace8dce3638e847b04e975489d1.jpg_750x200_a2dcb0a4.jpg'
+            },{
+                id: '0002',
+                imgUrl: 'http://mp-piao-admincp.qunarzz.com/mp_piao_admin_mp_piao_admin/admin/20199/c3635f8f6ac6a40f5069cd1b2b896100.jpg_750x200_0e548e3b.jpg'
+            }]
+        }
+    }
+}
+</script>
+
+<style scoped lang='stylus'>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background-color #fff 
+    .wrapper
+        overflow hidden
+        width 100%
+        height 0
+        padding-bottom 26.9%
+        background-color #eee
+        .swiper-img
+            width 100%
+</style>
