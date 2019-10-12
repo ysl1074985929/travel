f1dec860e65c0522f92ae78b5fbe6da3e2ceec43
yushilong <1074985929@qq.com>
2019-10-12 10:23:55 +0800

    header animation

--- src/pages/detaile/Detaile.vue
+++ src/pages/detaile/Detaile.vue
@@ -1,19 +1,24 @@
 <template>
     <div>
         <detaile-banner></detaile-banner>
+        <detaile-header></detaile-header>
+        <div class="content"></div>
     </div>
 </template>
 
 <script>
 import DetaileBanner from './components/Banner'
+import DetaileHeader from './components/Header'
 export default {
     name: 'Detaile',
     components: {
-        DetaileBanner
+        DetaileBanner,
+        DetaileHeader
     }
 }
 </script>
 
 <style lang='stylus' scoped>
-
+     .content
+        height 50rem
 </style>
